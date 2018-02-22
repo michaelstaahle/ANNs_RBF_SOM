50747eb15a9f2eece847ceb2eebd763f0f20af77
dbkh95 <patwal@kth.se>
2018-02-22 17:00:56 +0100

    made W part of grid

--- annSOM.py
+++ annSOM.py
@@ -6,24 +6,26 @@ class annSOM():
     def __init__(self, data, test_data):
         self.data = data
         self.W = None
-        self.grid = None
+        self.grid = None #W is grid[i,j]
         self.test_data = test_data
+        self.neighbourhood = None
 
 
-    def wdist(self,x):
-        dist=(x-self.W).dot(x-self.W)
+    def wdist(self,weight):
+        dist=(self.data[1]-weight).dot(self.data[1]-weight)
         return dist
 
     def update_weights(self,eta):
-        delta_w=eta*(self.data[1]-self.W)
-        self.W=self.W+delta_w
+        for i in neighbourhood:
+            delta_w=eta*(self.data[1]-self.grid[i])
+            self.grid[i]=self.W+delta_w
 
     def winner(self, data):
-        mindist=1000; #kan förmodligen ordnas på ett säkrare sätt
+        mindist=wdist(grid[0])
         minindex=-1
         for i in range(grid.size):
-            if wdist(x)<min:
-                mindist=wdist(x)
+            if wdist(grid[i])<min:
+                mindist=wdist(grid[i])
                 minindex=i
         return minindex
 

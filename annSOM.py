adb1dd81c050ebfd64accf196a2d99e180f2ffba
dbkh95 <patwal@kth.se>
2018-02-22 18:03:22 +0100

    extra comments+started neighbour finder

--- annSOM.py
+++ annSOM.py
@@ -8,19 +8,19 @@ class annSOM():
         self.W = None
         self.grid = None #W is grid[i,j]
         self.test_data = test_data
-        self.neighbourhood = None
+        self.neighbourhood = None #list of the relevant nodes to be updated
 
 
-    def wdist(self,weight):
+    def wdist(self,weight): #determines the distance between grid point and input, called by winner()
         dist=(self.data[1]-weight).dot(self.data[1]-weight)
         return dist
 
     def update_weights(self,eta):
-        for i in neighbourhood:
+        for i in self.neighbourhood:
             delta_w=eta*(self.data[1]-self.grid[i])
             self.grid[i]=self.W+delta_w
 
-    def winner(self, data):
+    def winner(self, data): #finds the index of the winning point
         mindist=wdist(grid[0])
         minindex=-1
         for i in range(grid.size):
@@ -29,3 +29,6 @@ class annSOM():
                 minindex=i
         return minindex
 
+    def find_neighbours(self,winner,dist):
+        nbrs=np.array(winner)
+        np.append(nbrs,neighbour) #TODO: add which the neighbour indices are
